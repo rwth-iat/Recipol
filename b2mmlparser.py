7d7526d3a1c34d0e229f6ba51d8a770d3eb2af2c
a-eve <alicia.eve@rwth-aachen.de>
2024-11-22 10:11:23 +0100

    added requirement class to b2mmlparser

--- b2mmlparser.py
+++ b2mmlparser.py
@@ -4,6 +4,24 @@ from defusedxml.ElementTree import parse
 ### static variables
 TESTXML = r"Artefakte\2024-11-03_MasterRecipeHC.xml"
 
+### classes
+class Requirement:
+    def __init__(self, id:str = "", const:str = ""):
+        self.id = id # unique identifier of the requirement
+        self.const = const # constraint of the requirement
+
+    def nameRequirement(self, id):
+        if self.id != "":
+            print("Cannot change id of existing requirement.")
+        else:
+            self.id = id
+
+    def addConstraint(self, const):
+        if self.const != "":
+            print("Cannot overwrite existing constraint of requirement.")
+        else:
+            self.const = const
+
 ### start main
 tree = parse(TESTXML)
 root = tree.getroot()
