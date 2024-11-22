0d117470309947c09ad2f4d5b60fc33f89fa63b9
a-eve <alicia.eve@rwth-aachen.de>
2024-11-22 11:44:33 +0100

    added parameter class to b2mmlparser

--- b2mmlparser.py
+++ b2mmlparser.py
@@ -30,21 +30,66 @@ class Requirement:
         else:
             self.const = const
 
+class Parameter:
+    def __init__(self, id:str = "", value:str = "", dtype:str = "", unit:str = ""):
+        self.id = id # unique identifier of the parameter
+        self.value = value # value of the paraemter
+        self.dtype = dtype # datatype of the parameter
+        self.unit = unit # measurement unit of the parameter
+
+    def __str__(self):
+        return f"ID: {self.id}, DATATYPE: {self.dtype}, VALUE: {self.value} {self.unit}"
+    
+    def nameParameter(self, id:str):
+        """Adds a unique identifier to the parameter."""
+
+        if self.id != "":
+            print("Cannot change id of existing requirement.")
+        else:
+            self.id = id
+
+    def addValue(self, value:str):
+        """Adds a value to the parameter."""
+
+        self.value = value
+
+    def addDataType(self, dtype:str):
+        """Adds a datatype to the parameter."""
+        
+        self.dtype = dtype
+
+    def addUnit(self, unit:str):
+        """Adds a unit of measurement to the parameter."""
+
+        self.unit = unit
+
 class Bml:
-    def __init__(self, reqs:list[Requirement] = []):
+    def __init__(self, reqs:list[Requirement] = [], params:list[Parameter] = []):
         self.reqs = reqs # list of requirements of the b2mml file
+        self.params = params # list of parameters of the b2mml file
 
     def __str__(self):
         descr = "Requirements:\n"
+
         for req in self.reqs:
-            descr += str(req)
+            descr += f"{str(req)}\n"
+
+        descr += "\nParameters:\n"
+
+        for param in self.params:
+            descr += f"{str(param)}\n"
 
         return descr
 
     def addRequirement(self, req:Requirement):
-        """Adds a requirement to the b2mml file."""
+        """Adds a requirement to the bml instance."""
         
         self.reqs.append(req)
+
+    def addParameter(self, param:Parameter):
+        """Adds a parameter to the bml instance."""
+
+        self.params.append(param)
 
 ### functions
 def parseMasterRecipe(node):
@@ -52,7 +97,7 @@ def parseMasterRecipe(node):
         if child.tag == f"{NAMESPACE}EquipmentRequirement":
             # create requirement object
             req = Requirement()
-            
+
             # fetch information about requirement
             for gchild in child:
                 if gchild.tag == f"{NAMESPACE}ID":
@@ -61,7 +106,22 @@ def parseMasterRecipe(node):
                     req.addConstraint(gchild.findtext(f"{NAMESPACE}Condition"))
 
             # add requirement to bml's requirements
-            bml.addRequirement(req)            
+            bml.addRequirement(req)
+
+        elif child.tag == f"{NAMESPACE}Formula":
+            for gchild in child:
+                if gchild.tag == f"{NAMESPACE}Parameter":
+                    # create parameter object
+                    param = Parameter()
+
+                    # fetch information about parameter
+                    param.nameParameter(gchild.findtext(f"{NAMESPACE}ID"))
+                    param.addValue(gchild.find(f"{NAMESPACE}Value").findtext(f"{NAMESPACE}ValueString"))
+                    param.addDataType(gchild.find(f"{NAMESPACE}Value").findtext(f"{NAMESPACE}DataType"))
+                    param.addUnit(gchild.find(f"{NAMESPACE}Value").findtext(f"{NAMESPACE}UnitOfMeasure"))
+
+                    # add parameter to bml's parameters
+                    bml.addParameter(param)
 
 ### start main
 
