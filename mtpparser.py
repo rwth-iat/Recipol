ff83721be8f0d8c886201ebe89442b7cb9232cf2
Alicia Eve <alicia.eve@rwth-aachen.de>
2025-03-27 09:30:50 +0100

    implementation 27-03

--- mtpparser.py
+++ mtpparser.py
@@ -1,9 +1,9 @@
 from defusedxml.ElementTree import parse
 
 ### static variables
-TESTMTP1 = r"Artefakte\HC10_manifest.aml"
+TESTMTP1 = r"Artefakte\HC10_new.aml"
 TESTMTP2 = r"Artefakte\HC30_manifest.aml"
-TESTMTPS = [TESTMTP1, TESTMTP2] 
+TESTMTPS = [TESTMTP1] 
 NAMESPACE = "{http://www.dke.de/CAEX}"
 
 ### classes
